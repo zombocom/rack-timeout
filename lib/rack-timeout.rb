9d97960fb415e900f502aad2109d0badbfbce0da
Caio Chassot <dev@caiochassot.com>
2013-03-12 23:56:18 +0100

    Insert rack-timeout before everything else in rails

--- lib/rack-timeout.rb
+++ lib/rack-timeout.rb
@@ -2,10 +2,10 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'rack/timeout')
 
 if defined? Rails
   case Rails::VERSION::MAJOR
-  when 2; Rails.configuration.middleware.use Rack::Timeout
+  when 2; Rails.configuration.middleware.insert 0, Rack::Timeout
   when 3
     class Rack::Timeout::Railtie < Rails::Railtie
-      initializer("rack-timeout.insert-rack-timeout") { |app| app.config.middleware.use Rack::Timeout }
+      initializer("rack-timeout.insert-rack-timeout") { |app| app.config.middleware.insert 0, Rack::Timeout }
     end
   end
 end
