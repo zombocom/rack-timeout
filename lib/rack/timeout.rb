d75926bf4092d13f5a13b3885f597d02a6c0844b
Caio Chassot <dev@caiochassot.com>
2013-04-24 17:13:41 +0200

    set timeout according to request age

--- lib/rack/timeout.rb
+++ lib/rack/timeout.rb
@@ -18,8 +18,11 @@ module Rack
     def call(env)
       request_start = env["HTTP_X_REQUEST_START"] # Unix timestamp in ms
       request_start &&= Time.at request_start.to_i / 1000.0
-      raise RequestDroppedByRouterError if Time.now - request_start > 30
-      ::Timeout.timeout(self.class.timeout, ::Rack::Timeout::Error) { @app.call(env) }
+      request_age = Time.now - request_start
+      time_left = 30 - request_age
+      raise RequestDroppedByRouterError if time_left < 0
+      timeout = [self.class.timeout, time_left].min
+      ::Timeout.timeout(timeout, ::Rack::Timeout::Error) { @app.call(env) }
     end
 
   end
