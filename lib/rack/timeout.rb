77a02987a3b753593719a5ed81d6e44d545e98e7
Caio Chassot <dev@caiochassot.com>
2013-04-24 17:28:46 +0200

    logging!

--- lib/rack/timeout.rb
+++ lib/rack/timeout.rb
@@ -15,14 +15,30 @@ module Rack
       @app = app
     end
 
+    def f n
+      '%05.2f' % n
+    end
+
     def call(env)
-      request_start = env["HTTP_X_REQUEST_START"] # Unix timestamp in ms
+      request_id      = env['HTTP_HEROKU_REQUEST_ID'].to_s[0, 6]
+      request_start   = env["HTTP_X_REQUEST_START"] # Unix timestamp in ms
       request_start &&= Time.at request_start.to_i / 1000.0
-      request_age = Time.now - request_start
-      time_left = 30 - request_age
-      raise RequestDroppedByRouterError if time_left < 0
+      request_age     = Time.now - request_start
+      time_left       = 30 - request_age
+      if time_left < 0
+        $stderr.puts "rack-timeout: id=#{request_id} age=#{f request_age} aborted, too old"
+        raise RequestDroppedByRouterError
+      end
       timeout = [self.class.timeout, time_left].min
-      ::Timeout.timeout(timeout, ::Rack::Timeout::Error) { @app.call(env) }
+      $stderr.puts "rack-timeout: id=#{request_id} age=#{f request_age} timeout=#{f timeout}"
+      ::Timeout.timeout(timeout, ::Rack::Timeout::Error) do
+        t0 = Time.now
+        retval = @app.call(env)
+        t1 = Time.now
+        td = t1 - t0
+        $stderr.puts "rack-timeout: id=#{request_id} age=#{f request_age} timeout=#{f timeout} completed=#{f td}"
+        retval
+      end
     end
 
   end
