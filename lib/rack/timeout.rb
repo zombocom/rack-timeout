407f3bf550b0dcf595f582faa4cb580bbaea08c4
Caio Chassot <dev@caiochassot.com>
2013-04-26 18:57:22 +0200

    a bunch of things, important things:
    - Some useful logging around the request lifetime
      - logging Heroku's request_id
      - still no tracking of timeouts raised inside frameworks
    - Aware of Heroku's X-Request-Start header, uses it to determine maximum useful timeout (Heroku drops requests in the router after 30s)
    - Rack::Timeout::Error no longer a subclass of Timeout::Error, instead simply of RuntimeError, because…
    - raising two different errors now: RequestTooOldError and RequestAbortedError, which subclass the new Rack::Timeout::Error
      - RequestAbortedError is raised where the old (Rack::)Timeout::Error was raised
      - RequestTooOldError is raised when X-Request-Start is present and too old
    - Information that could be useful outside of rack-timeout, like age and timeout, is made available in env

--- lib/rack/timeout.rb
+++ lib/rack/timeout.rb
@@ -3,7 +3,9 @@ require 'timeout'
 
 module Rack
   class Timeout
-    class Error < ::Timeout::Error; end
+    class Error < RuntimeError;        end
+    class RequestTooOldError  < Error; end
+    class RequestAbortedError < Error; end
 
     @timeout = 15
     class << self
@@ -14,8 +16,50 @@ module Rack
       @app = app
     end
 
+    MAX_REQUEST_AGE = 30 # seconds
     def call(env)
-      ::Timeout.timeout(self.class.timeout, ::Rack::Timeout::Error) { @app.call(env) }
+      request_start = env['HTTP_X_REQUEST_START'] # unix timestamp in ms
+      request_start = Time.at(request_start.to_i / 1000) if request_start
+      request_age   = Time.now - request_start           if request_start
+      time_left     = MAX_REQUEST_AGE - request_age      if request_age
+      timeout       = [self.class.timeout, time_left].compact.min
+
+      env['rack-timeout.request-age'] = request_age
+      env['rack-timeout.timeout']     = timeout if timeout > 0
+
+      if timeout <= 0
+        log env, "dropped"
+        raise RequestTooOldError
+      end
+
+      log env, "starting"
+      ::Timeout.timeout(timeout, RequestAbortedError) do
+        t0 = Time.now
+        rv = @app.call(env)
+        td = Time.now - t0
+        env['rack-timeout.completed'] = td
+        log env
+        rv
+      end
+    end
+
+    private
+
+    def log(env, msg=nil)
+      id, age, timeout, completed = env.values_at \
+        *%w[ HTTP_HEROKU_REQUEST_ID rack-timeout.request-age rack-timeout.timeout rack-timeout.completed ]
+      s = "rack-timeout:"
+      s << " id="        << id if id
+      s << " age="       << ms(age) if age
+      s << " timeout="   << ms(timeout) if timeout
+      s << " completed=" << ms(completed) if completed
+      s << " "           << msg if msg
+      s << "\n"
+      $stderr << s
+    end
+
+    def ms(s)
+      "%.fms" % (s * 1000) if s
     end
 
   end
