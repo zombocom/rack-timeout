26fb31add1dac36fe480637bc5ab33b39b35381a
Caio Chassot <dev@caiochassot.com>
2013-04-26 16:45:00 +0200

    add rails 4 support

--- lib/rack-timeout.rb
+++ lib/rack-timeout.rb
@@ -3,7 +3,7 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'rack/timeout')
 if defined? Rails
   case Rails::VERSION::MAJOR
   when 2; Rails.configuration.middleware.insert 0, Rack::Timeout
-  when 3
+  when 3, 4
     class Rack::Timeout::Railtie < Rails::Railtie
       initializer("rack-timeout.insert-rack-timeout") { |app| app.config.middleware.insert 0, Rack::Timeout }
     end
