83ca9f5141c1fdcb626820b1601c406e3a3a560a
Caio Chassot <dev@caiochassot.com>
2013-07-02 04:18:22 +0200

    Don't rely on rack.input to check for a body, just check the http headers instead. fixes #21

--- lib/rack/timeout.rb
+++ lib/rack/timeout.rb
@@ -27,9 +27,8 @@ module Rack
       request_start = env['HTTP_X_REQUEST_START'] # unix timestamp in ms
       request_start = Time.at(request_start.to_f / 1000) if request_start
       info.age      = Time.now - request_start           if request_start
-      has_body      = env["rack.input"].size > 0         if env["rack.input"]
       time_left     = MAX_REQUEST_AGE - info.age         if info.age
-      time_left    += self.class.overtime                if time_left && has_body
+      time_left    += self.class.overtime                if time_left && self.class._request_has_body?(env)
       info.timeout  = [self.class.timeout, time_left].compact.select { |n| n >= 0 }.min
 
       if time_left && time_left <= 0
@@ -69,6 +68,13 @@ module Rack
       raise "Invalid state: #{state.inspect}" unless VALID_STATES.include? state
       env[ENV_INFO_KEY].state = state
       notify_state_change_observers(env)
+    end
+
+    def self._request_has_body?(env)
+      return true  if env['HTTP_TRANSFER_ENCODING'] == 'chunked'
+      return false if env['CONTENT_LENGTH'].nil?
+      return false if env['CONTENT_LENGTH'].to_i.zero?
+      true
     end
 
 
