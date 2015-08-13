c080dcf2618c542c0267c6212602a6a6b7422227
Caio Chassot <caio@heroku.com>
2015-08-13 19:38:02 +0200

    standardize on double quotes for everything

--- lib/rack/timeout/rails.rb
+++ lib/rack/timeout/rails.rb
@@ -1,5 +1,5 @@
 require_relative "base"
 
 class Rack::Timeout::Railtie < Rails::Railtie
-  initializer('rack-timeout.prepend') { |app| app.config.middleware.insert 0, Rack::Timeout }
+  initializer("rack-timeout.prepend") { |app| app.config.middleware.insert 0, Rack::Timeout }
 end
