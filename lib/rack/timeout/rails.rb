18a08ab2e0fcd3f0565cd428c46f703464919299
Ravi Asnani <asnafam@gmail.com>
2019-10-02 14:47:35 +0530

    Load middleware in Rails 6

--- lib/rack/timeout/rails.rb
+++ lib/rack/timeout/rails.rb
@@ -4,10 +4,12 @@ class Rack::Timeout::Railtie < Rails::Railtie
   initializer("rack-timeout.prepend") do |app|
     next if Rails.env.test?
 
+    middleware = Rails::VERSION::MAJOR >= 6 ? app.middleware : app.config.middleware
+
     if defined?(ActionDispatch::RequestId)
-      app.config.middleware.insert_after(ActionDispatch::RequestId, Rack::Timeout)
+      middleware.insert_after(ActionDispatch::RequestId, Rack::Timeout)
     else
-      app.config.middleware.insert_before(Rack::Runtime, Rack::Timeout)
+      middleware.insert_before(Rack::Runtime, Rack::Timeout)
     end
   end
 end
