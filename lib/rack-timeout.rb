ea4e1872266be916b14baccac4426a110eff5c26
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2022-05-27 10:11:31 +0000

    Support Rails 7

--- lib/rack-timeout.rb
+++ lib/rack-timeout.rb
@@ -1,2 +1,9 @@
 require_relative "rack/timeout/base"
-require_relative "rack/timeout/rails" if defined?(Rails) && [3,4,5,6].include?(Rails::VERSION::MAJOR)
+
+if defined?(Rails)
+  if [3,4,5,6,7].include?(Rails::VERSION::MAJOR)
+    require_relative "rack/timeout/rails"
+  else
+    warn "RACK-TIMEOUT ******************** UNSUPPORTED RAILS VERSION: #{Rails::VERSION::MAJOR}"
+  end
+end
